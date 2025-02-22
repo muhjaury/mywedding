ed4cd647d82fda030bb986411a5afd974c4acda1
Muhammad Jaury <muhjaury@gmail.com>
2025-02-22 21:19:26 +0700

    Fix flex-grow Gallery

--- src/components/ModalContent/GalleryContent/_galleryContent.tsx
+++ src/components/ModalContent/GalleryContent/_galleryContent.tsx
@@ -8,13 +8,18 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   overflow-y: scroll;
-  display: flex;
-  flex-wrap: wrap;
-  flex-grow: 1;
 
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+export const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-grow: 1;
+  flex-wrap: nowrap;
 `;
 
 export const Img = styled.img<IF_Image>`
@@ -22,4 +27,9 @@ export const Img = styled.img<IF_Image>`
   width: 20%;
   object-fit: cover;
   vertical-align: bottom;
+  transition: 0.3s;
+
+  &:hover {
+    width: 40%;
+  }
 `;
--- src/components/ModalContent/GalleryContent/index.tsx
+++ src/components/ModalContent/GalleryContent/index.tsx
@@ -1,29 +1,37 @@
-import { Img, Wrapper } from "./_galleryContent";
+import { Img, ImgWrapper, Wrapper } from "./_galleryContent";
 import { Images } from "./images";
 
 function GalleryContent() {
   return (
     <Wrapper>
-      <Img src={Images.IMG1} alt="img1" />
-      <Img src={Images.IMG2} alt="img2" />
-      <Img src={Images.IMG3} alt="img3" />
-      <Img src={Images.IMG4} alt="img4" />
-      <Img src={Images.IMG5} alt="img5" />
-      <Img src={Images.IMG6} alt="img6" />
-      <Img src={Images.IMG7} alt="img7" />
-      <Img src={Images.IMG8} alt="img8" />
-      <Img src={Images.IMG9} alt="img9" />
-      <Img src={Images.IMG10} alt="img10" />
-      <Img src={Images.IMG11} alt="img11" />
-      <Img src={Images.IMG12} alt="img12" />
-      <Img src={Images.IMG13} alt="img13" />
-      <Img src={Images.IMG14} alt="img14" />
-      <Img src={Images.IMG15} alt="img15" />
-      <Img src={Images.IMG16} alt="img16" />
-      <Img src={Images.IMG17} alt="img17" />
-      <Img src={Images.IMG18} alt="img18" />
-      <Img src={Images.IMG19} alt="img19" />
-      <Img src={Images.IMG20} alt="img20" />
+      <ImgWrapper>
+        <Img src={Images.IMG1} alt="img1" />
+        <Img src={Images.IMG2} alt="img2" />
+        <Img src={Images.IMG3} alt="img3" />
+        <Img src={Images.IMG4} alt="img4" />
+        <Img src={Images.IMG5} alt="img5" />
+      </ImgWrapper>
+      <ImgWrapper>
+        <Img src={Images.IMG6} alt="img6" />
+        <Img src={Images.IMG7} alt="img7" />
+        <Img src={Images.IMG8} alt="img8" />
+        <Img src={Images.IMG9} alt="img9" />
+        <Img src={Images.IMG10} alt="img10" />
+      </ImgWrapper>
+      <ImgWrapper>
+        <Img src={Images.IMG11} alt="img11" />
+        <Img src={Images.IMG12} alt="img12" />
+        <Img src={Images.IMG13} alt="img13" />
+        <Img src={Images.IMG14} alt="img14" />
+        <Img src={Images.IMG15} alt="img15" />
+      </ImgWrapper>
+      <ImgWrapper>
+        <Img src={Images.IMG16} alt="img16" />
+        <Img src={Images.IMG17} alt="img17" />
+        <Img src={Images.IMG18} alt="img18" />
+        <Img src={Images.IMG19} alt="img19" />
+        <Img src={Images.IMG20} alt="img20" />
+      </ImgWrapper>
     </Wrapper>
   );
 }
