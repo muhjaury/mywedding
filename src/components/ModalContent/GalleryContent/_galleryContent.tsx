414b38742d2c84252087ac91c0bdc2c8bdfad883
Muhammad Jaury <muhjaury@gmail.com>
2025-02-22 21:37:30 +0700

    Fix Gallery for mobile view

--- src/components/ModalContent/GalleryContent/_galleryContent.tsx
+++ src/components/ModalContent/GalleryContent/_galleryContent.tsx
@@ -1,3 +1,4 @@
+import { color } from "@/constant/color";
 import styled from "styled-components";
 
 interface IF_Image {
@@ -8,28 +9,49 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   overflow-y: scroll;
+  overflow-x: scroll;
 
   &::-webkit-scrollbar {
-    display: none;
+    width: 8px;
+    height: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: ${color.a4};
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${color.a2};
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: ${color.a4};
   }
 `;
 
 export const ImgWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+  justify-content: start;
   flex-grow: 1;
   flex-wrap: nowrap;
 `;
 
 export const Img = styled.img<IF_Image>`
   height: auto;
-  width: 20%;
+  width: 30%;
   object-fit: cover;
   vertical-align: bottom;
   transition: 0.3s;
-
   &:hover {
     width: 40%;
+  }
+
+  @media all and (min-width: 1024px) {
+    width: 20%;
+
+    &:hover {
+      width: 40%;
+    }
   }
 `;
